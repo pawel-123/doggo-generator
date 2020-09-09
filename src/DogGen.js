7b55f870f7c687496393123271f5910ed3a21b0c
pawel_admin <ptcebula@gmail.com>
2020-09-09 20:59:28 +0200

    Removed unused UseEffect and edited README.md

--- src/DogGen.js
+++ src/DogGen.js
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function DogGen() {
 
-  const initialUrl = "https://images.dog.ceo/breeds/dingo/n02115641_6772.jpg"
+    const initialUrl = "https://images.dog.ceo/breeds/dingo/n02115641_6772.jpg"
 
-  const [url, setUrl] = useState(initialUrl);
+    const [url, setUrl] = useState(initialUrl);
 
-  function generate() {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then(response => response.json())
-      .then(data => {
-        setUrl(data.message)
-      })
-  }
+    function generate() {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => {
+                setUrl(data.message)
+            })
+    }
 
-  return (
-    <div className="generator">
-      <button onClick={generate}>NEW DOGGO</button>
-      <img src={url} alt='random doggo of the day' />
-    </div>
-  );
+    return (
+        <div className="generator">
+            <button onClick={generate}>NEW DOGGO</button>
+            <img src={url} alt='random doggo of the day' />
+        </div>
+    );
 }
 
 export default DogGen;
